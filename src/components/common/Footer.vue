7a4187ba48c3bf1dc8ac80cb585941315d521e21
Cibles <122311693+c1bles@users.noreply.github.com>
2025-04-18 22:44:36 +0100

    Footer Links UPD

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="m-auto w-full py-3 px-4 sm:px-6 mx-auto mt-8">
     <div class="flex justify-between items-center gap-5">
-      <p class="text-xs">Made in Manchester by <a href="https://www.kevan.tv" target="_blank" class="underline">Matt Kevan</a>.</p>
-      <p class="text-xs"><a href="https://github.com/MattKevan/minimal-reader/" target="_blank" class="underline">Github</a></p>
+      <p class="text-xs">Fork made by <a href="c1bles.github.io" target="_blank" class="underline">Cibles</a>.</p>
+      <p class="text-xs"><a href="https://github.com/c1bles/nominal-reader/" target="_blank" class="underline">Github</a></p>
     </div>
   </footer>
 </template>
