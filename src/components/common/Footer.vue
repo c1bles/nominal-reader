3fed0aacc3cb0c8773f655a32e83a9f91e453e4e
Cibles <122311693+c1bles@users.noreply.github.com>
2025-04-19 16:53:51 +0100

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -2,8 +2,7 @@
   <footer class="m-auto w-full py-3 px-4 sm:px-6 mx-auto mt-8">
     <div class="flex justify-between items-center gap-5">
       <p class="text-xs">Fork made by <a href="https://c1bles.github.io/" target="_blank" class="underline">Cibles</a>.</p>
-      <p class="text-xs"><a href="https://github.com/c1bles/nominal-reader/" target="_blank" class="underline">Github</a></p>
-      <p class="text-xs"><a href="https://discord.gg/nqzP6VKwMs" target="_blank" class="underline">Discord</a></p>
+      <a href="https://github.com/c1bles/nominal-reader/" target="_blank" class="underline">Github</a> <p class="text-xs">, <a href="https://discord.gg/nqzP6VKwMs" target="_blank" class="underline">Discord</a>.</p>
     </div>
   </footer>
 </template>
