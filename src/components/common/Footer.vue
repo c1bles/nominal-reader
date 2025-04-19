3866e07625fc9edc9ebbc0c8837693a12d95afb1
Cibles <122311693+c1bles@users.noreply.github.com>
2025-04-19 15:16:10 +0100

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,6 +3,7 @@
     <div class="flex justify-between items-center gap-5">
       <p class="text-xs">Fork made by <a href="https://c1bles.github.io/" target="_blank" class="underline">Cibles</a>.</p>
       <p class="text-xs"><a href="https://github.com/c1bles/nominal-reader/" target="_blank" class="underline">Github</a></p>
+      <p class="text-xs"><a href="" target="_blank" class="underline">Github</a></p>
     </div>
   </footer>
 </template>
