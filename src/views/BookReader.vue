00be697ac2b8a34f3d1810bcf4f6f41e7f5e452f
Cibles <122311693+c1bles@users.noreply.github.com>
2025-05-14 19:21:11 +0100

    Small Contents Text Size

--- src/views/BookReader.vue
+++ src/views/BookReader.vue
@@ -33,7 +33,7 @@
         <h3 class="font-bold text-gray-800 dark:text-white">
           Contents
         </h3>
-        <button type="button" class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" data-hs-overlay="#hs-overlay-right">
+        <button type="button" class="flex justify-center items-center size-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" data-hs-overlay="#hs-overlay-right">
           <span class="sr-only">Close modal</span>
           <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M18 6 6 18"></path>
